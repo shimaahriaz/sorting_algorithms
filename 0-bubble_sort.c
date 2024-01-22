e58a270d37b71642dbba9a0cedab1c7ce2cbb8d6
shimaahriaz <shimaahriaz@gmail.com>
2024-01-22 07:27:54 -0800

    a

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,35 +3,34 @@
 /**
  * bubble_sort - Sorts an array of integers in ascending order
  * using the Bubble sort algorithm
- * @array: The array to be sorted
+ * @array: The array to be printed
  * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 1;
-	int temp, flag = 1;
+    size_t i;
+    int tmp;
+    int flag = 1;
 
-	if (!array || size < 2)
-		return;
+    if (!array || size < 2)
+        return;
 
-	while (flag == 1)
-	{
-		flag = 0;
-		i = 0;
-
-		while (i < size - j)
-		{
-			if (array[i] > array[i + 1])
-			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-				print_array(array, size);
-				flag = 1;
-			}
-			i++;
-}
-		j++;
-	}
+    while (flag == 1)
+    {
+        flag = 0;
+        i = 0;
+        while (i < size - 1)
+        {
+            if (array[i] > array[i + 1])
+            {
+                tmp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = tmp;
+                print_array(array, size);
+                flag = 1;
+            }
+            i++;
+        }
+    }
 }
 
